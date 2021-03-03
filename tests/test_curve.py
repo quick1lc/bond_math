ef8f318e7d96d791f98630328c6913eb0feb1bbf
quick1lc <lc@yarnelle.com>
2021-03-03 08:11:50 -0500

    Learning more about unit tests; no useful updates

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -1,8 +1,10 @@
 import unittest
+import pandas as pd
 from bond_math import curve
 
 term = [1,3,5,7,10]
 spot = [1,2,3,4,5]
+# series = pd.Series(data=spot, index=term)
 
 class curve_test(unittest.TestCase):
     def test_frange(self):
@@ -11,3 +13,10 @@ class curve_test(unittest.TestCase):
         self.assertEqual(c._frange(2,4,1),[2,3])
         self.assertNotEqual(c._frange(1.5,4.0,0.5),[2,3,4])
         self.assertNotEqual(c._frange(2,4,1),[2,3,4])
+
+    # def test_calc_discount_factors(self):
+    #     c = curve(term_vector=term, spot_vector=spot)
+    #     pd.testing.assert_series_equal(c._calc_discount_factors(series, 2, 100),series)
+
+    # def test_add_discount_factors(self):
+    #     c = curve(term_vector=term, spot_vector=spot)
